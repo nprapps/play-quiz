1634fc7b2fde3dff397001b4db0d15bb5783a6d6
Tyler Fisher <tyler@tylerjfisher.com>
2014-07-22 11:29:13 -0400

    find the primary and secondary category and get the result out of the matrix

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,17 @@ var $questions = null;
 // Global state
 var firstShareLoad = true;
 var category = null;
+var primaryCategory = null;
+var secondaryCategory = null;
+var games = null;
+
+var categories = {
+    'creativity': 0,
+    'social_connection': 0,
+    'physical_exploration': 0,
+    'pleasure_fun': 0,
+    'mental_stimulation': 0
+}
 
 /*
  * Run on page load.
@@ -23,7 +34,6 @@ var onDocumentLoad = function(e) {
     $shareModal.on('hidden.bs.modal', onShareModalHidden);
     $submitQuiz.on('click', calculateResult);
 
-    // renderExampleTemplate();
     getCommentCount(showCommentCount);
 }
 
@@ -31,8 +41,29 @@ var calculateResult = function() {
     _.each($questions, function(question) {
         findAnswer(question);
 
-        console.log(category);
-    })
+        categories[category]++;
+    });
+
+    // take the categories object and make sort-of tuples in JavaScript lol.
+    var tuples = [];
+
+    for (var key in categories) {
+        tuples.push([key, categories[key]]);
+    }
+
+    // sort the tuple from greatest to least
+    tuples.sort(function(a, b) {
+        a = a[1];
+        b = b[1];
+
+        return a > b ? -1 : (a < b ? 1 : 0);
+    });
+
+    // the primary category is the first in the list, the secondary is the second.
+    primaryCategory = tuples[0][0]
+    secondaryCategory = tuples[1][0];
+
+    printResult(primaryCategory, secondaryCategory);
 }
 
 var findAnswer = function(question) {
@@ -42,23 +73,46 @@ var findAnswer = function(question) {
         if ($(answer).is(':checked')) {
             category = $(answer).attr('value');
         }
-    })
+    });
 }
 
-/*
- * Basic templating example.
- */
-// var renderExampleTemplate = function() {
-//     var context = $.extend(APP_CONFIG, {
-//         'template_path': 'jst/example.html',
-//         'config': JSON.stringify(APP_CONFIG, null, 4),
-//         'copy': JSON.stringify(COPY, null, 4)
-//     });
+var printResult = function(primary, secondary) {
+    // find the result based on primary and secondary categories
+    var games = COPY.games;
 
-//     var html = JST.example(context);
+    var primaryArray = [];
 
-//     $('#template-example').html(html);
-// }
+    for (i=0; i<games.length; i++) {
+        var categoryArray = games[i]
+
+        if (categoryArray[0] === primary) {
+            primaryArray = categoryArray;
+        }
+    }
+
+    var result = null;
+
+    switch(secondary) {
+        case 'creativity':
+            result = primaryArray[1];
+            break;
+        case 'social_connection':
+            result = primaryArray[2];
+            break;
+        case 'physical_exploration':
+            result = primaryArray[3];
+            break;
+        case 'pleasure_fun':
+            result = primaryArray[4];
+            break;
+        case 'mental_stimulation':
+            result = primaryArray[5];
+            break;
+    }
+
+    $('.result').text(result);
+
+}
 
 /*
  * Display the comment count.
